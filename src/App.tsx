55bd954c955e2f1fcb0d1d6c03d8fb4ef139edd0
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-11-06 01:14:21 +0300

    Cтатистические данные ( логика)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+
 import React from 'react';
 import {Product} from './component/Product';
 import {products} from './data/products'
 function App() {
 
-   const[count,setCount]= useState(0)
+   
   return (
    <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]}/>
-      <Product product={products[1] }/>
+
+     {products.map(product => <Product product={product} key={product.id}/>)}
+
+      {/*<Product product={products[0]}/>
+      <Product product={products[1] }/>*/} 
     
    {/*<h1>Test JS {count}</h1>
    <button  className="py-2  px-4">Click me</button>*/}
--- src/component/Product.tsx
+++ src/component/Product.tsx
@@ -9,18 +9,27 @@ export function Product({product}:ProductProps) {
    
    const [detalies, setDetailes] = useState(false)
    
+   const btnBgClassName = detalies ? 'bg-yellow-400 ' : 'bg-blue-400 '
+ 
+   const btnClasses = ['py-2 px-4 border', btnBgClassName]
+     
    return (
       <div className="border py-2 px-4  rounded flex flex-col items-center mb-2">
          <img src={product.image} className='w-1/6' alt={product.title}/>
         <p>{product.title}</p> 
         <p className='font-bold'>{product.price}</p>
         <button 
-        onClick={()=> setDetailes(true)}
-        className='py-2 px-4 border bg-yellow-400'>
-         Show ditalies
+        onClick={()=> setDetailes(prev=>!prev)}
+        className={btnClasses.join(' ')}>
+         {detalies ? 'Hide Details' : 'Show Details'}
          </button>
+      
+       
+
+
         {detalies && <div>
           <p>{product.description}</p>
+          <p>Rate:<span style={{fontWeight:'bold'}}>{product.rating.rate}</span></p>
          </div>}
        
          </div>
