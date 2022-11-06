56f8084a1982038bf1d539d799ec96497df55b33
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-11-07 01:57:22 +0300

    Добавил loading и ErrorAxios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import  {AxiosError}  from 'axios';
 import React, { useEffect,useState  } from 'react';
 import {Product} from './component/Product';
 import {products} from './data/products'
@@ -8,9 +9,24 @@ function App() {
 
    const [products, setProducts] =useState<IProduct[]>([])
 
+   const [loading, setLoading] = useState(false)
+
+   const [error, setError] = useState('')
+
    async function fetchProducts() {
-    const response =  await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-   setProducts(response.data)
+
+      try{
+         setError('')
+         setLoading(true)
+         const response =  await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+        setProducts(response.data)
+        setLoading(false)
+      } catch (e: unknown){
+         const error = e as AxiosError
+         setLoading(false)
+         setError(error.message)
+      }
+ 
    
    }
   
@@ -20,7 +36,8 @@ function App() {
    
   return (
    <div className="container mx-auto max-w-2xl pt-5">
-
+      {loading && <p className='text-center'>Loading...</p>}
+      {error && <p className='text-center text-red-600'>{error}</p>}
      {products.map(product => <Product product={product} key={product.id}/>)}
 
       {/*<Product product={products[0]}/>
