300d98a60349bd548cea26550f36c73801842887
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-11-07 17:56:41 +0300

    Рефакторинг кода, продолжение

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
+import { Product } from "./component/Product";
 
-import axios from 'axios';
-import  {AxiosError}  from 'axios';
-import React, { useEffect,useState  } from 'react';
-import {Product} from './component/Product';
-import {products} from './data/products'
-import { IProduct } from './models';
-import {useProducts} from './component/hooks/products'
-
+import { useProducts } from "./component/hooks/products";
+import { Loader } from "././component/Loader";
+import { ErrorMessage } from "./component/ErrorMessage";
 
 function App() {
+  const { loading, error, products } = useProducts();
 
-   const {loading, error, products} = useProducts()
-   
   return (
-   <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <p className='text-center'>Loading...</p>}
-      {error && <p className='text-center text-red-600'>{error}</p>}
-     {products.map(product => <Product product={product} key={product.id}/>)}
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
 
       {/*<Product product={products[0]}/>
-      <Product product={products[1] }/>*/} 
-    
-   {/*<h1>Test JS {count}</h1>
+      <Product product={products[1] }/>*/}
+
+      {/*<h1>Test JS {count}</h1>
    <button  className="py-2  px-4">Click me</button>*/}
-   </div>
+    </div>
   );
 }
 
